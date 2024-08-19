2b524a278fbfce76df9dd5627d948e7057997c95
Yana Mikhierieva <mikhereva@gmail.com>
2024-08-19 14:04:34 +0200

    update css, js files

--- src/main.js
+++ src/main.js
@@ -4,12 +4,18 @@ import { fetchImg } from "./js/pixabay-api";
 import { createGalleryCardTemplate } from "./js/render-functions";
 const form = document.querySelector('.js-search-form');
 const gallerySelected = document.querySelector('.js-gallery');
-// const loader = document.querySelector('.loader');
+const lightbox = new SimpleLightbox('.js-gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+        captions: true,
+        animationSpeed: 300,
+        close: true,
+});
+const loader = document.querySelector('.loader');
 const handleSubmit = event => {
     event.preventDefault();
-    const loader = document.querySelector('.loader');
-    // loader.classList.remove('is-hidden');
-    const searchInput = form.elements.user_query.value;
+    gallerySelected.innerHTML = '';
+    const searchInput = form.elements.user_query.value.trim();
     if (!searchInput) {
         return
     }
@@ -20,22 +26,23 @@ const handleSubmit = event => {
         title: 'Error',
         message: 'Sorry, there are no images matching your search query. Please try again!',
         position: 'topRight',
+        maxWidth: '400px',
+        backgroundColor: '#daff00',
             });
-            gallerySelected.innerHTML = '';
-            form.reset();
+            return;
         }
         const cardsGalleryList = data.hits.map(card => createGalleryCardTemplate(card)).join('');
         gallerySelected.innerHTML = cardsGalleryList;
-        const lightbox = new SimpleLightbox('.js-gallery a', {
-        captionsData: 'alt',
-        captionDelay: 250,
-        captions: true,
-        animationSpeed: 300,
-        close: true,
-        });
         lightbox.refresh();
     }).catch(err => {
         console.error(err);
+        iziToast.error({
+        title: 'Error',
+        message: 'Oops! Error! Try again later!',
+        position: 'topRight',
+        maxWidth: '400px',
+        backgroundColor: '#daff00',
+        });
     }).finally(() => {
         form.reset();
         loader.classList.add('is-hidden');
